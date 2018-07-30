bf39f8fb062f03935cb4c1f83369b6fb961cebb3
DaiyaKawasaki_c17006 <c17006@std.it-college.ac.jp>
2018-07-30 15:31:03 +0900

    Unityデータの追加

---
+++ BADBOYMISCHIEF/Assets/daiya/script/ItemTime.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemTime : MonoBehaviour {
+
+    public Image UIobj;
+    Player playercs;
+
+
+    void Start() {
+        playercs = GameObject.FindWithTag("Player").GetComponent<Player>();
+
+    }
+
+    void Update() {
+        if (gameObject.tag == "BakutikuButton")
+            BakutikuCal();
+
+        if (gameObject.tag == "GumButton")
+            GumCal();
+
+        if (gameObject.tag == "DogButton")
+            DogCal();
+
+        if (gameObject.tag == "CoinButton")
+            CoinCal();
+
+    }
+
+
+    void BakutikuCal() {
+        if (UIobj.fillAmount > 0) {
+            UIobj.fillAmount -= 1.0f / 2.5f * Time.deltaTime;
+
+        }
+
+        if (playercs.bakutikutime == 2.5f) {
+            UIobj.fillAmount = 1.0f;
+
+        }
+    }
+
+    void GumCal() {
+        if (UIobj.fillAmount > 0) {
+            UIobj.fillAmount -= 1.0f / 2.0f * Time.deltaTime;
+
+        }
+
+        if (playercs.gumtime == 2.0f) {
+            UIobj.fillAmount = 1.0f;
+
+        }
+    }
+
+    void DogCal() {
+        if (UIobj.fillAmount > 0) {
+            UIobj.fillAmount -= 1.0f / 5.0f * Time.deltaTime;
+
+        }
+
+        if (playercs.dogtime == 5.0f) {
+            UIobj.fillAmount = 1.0f;
+
+        }
+    }
+
+    void CoinCal() {
+        if (UIobj.fillAmount > 0) {
+            UIobj.fillAmount -= 1.0f / 5.0f * Time.deltaTime;
+
+        }
+        if (playercs.cointime == 5.0f) {
+            UIobj.fillAmount = 1.0f;
+
+        }
+    }
+}
---
+++ BADBOYMISCHIEF/Assets/daiya/script/TitleBGM.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleBGM : MonoBehaviour {
+
+    public bool DontDestroyEnabled = true;
+    private GameObject[] bgm;
+
+
+    void Start() {
+        bgm = GameObject.FindGameObjectsWithTag("BGM");
+        if (bgm.Length == 2) {
+            Destroy(gameObject);
+
+        }
+    }
+	
+	void Update () {
+        if (SceneManager.GetActiveScene().name == "Stage1" ||
+            SceneManager.GetActiveScene().name == "Stage2" ||
+            SceneManager.GetActiveScene().name == "Stage3" ||
+            SceneManager.GetActiveScene().name == "Stage4" ||
+            SceneManager.GetActiveScene().name == "Stage5") {
+            Destroy(gameObject);
+
+        }
+
+        if (DontDestroyEnabled) {
+            // Sceneを遷移してもオブジェクトが消えないようにする
+            DontDestroyOnLoad(this);
+        }
+    }
+}
